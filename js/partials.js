5e3b2ba2664450e04c9337fa3ab2e00583a57473
Gustavo Romero <gustavo@43t.com.br>
2021-02-17 11:58:59 -0300

    novo partial footer

--- js/partials.js
+++ js/partials.js
@@ -6,7 +6,7 @@ function loadPartials() {
 
   $('#ftco-navbar-topbar-container').load('partials/ftco-navbar-topbar-content.html')
 
-  // $('#header-navbar').load('partials/header-navbar.html')
+  $('#footer-container').load('partials/footer-content.html')
 
   // $('#footer-content').load('partials/footer-content.html')
 
