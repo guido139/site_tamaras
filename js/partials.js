0d15e960a84c4f76cc3984430898c2ee54954e88
Gustavo Romero <gustavo@43t.com.br>
2021-02-17 13:21:01 -0300

    Mudança imagem bgindex

--- js/partials.js
+++ js/partials.js
@@ -6,6 +6,7 @@ function loadPartials() {
 
   $('#ftco-navbar-topbar-container').load('partials/ftco-navbar-topbar-content.html')
 
+  $('#title-container').load('partials/title-content.html')
   $('#footer-container').load('partials/footer-content.html')
 
   // $('#footer-content').load('partials/footer-content.html')
