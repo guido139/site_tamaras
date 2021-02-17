12e0ab31509d06530f3eee0778ce69e6b15ee52d
Gustavo Romero <gustavo@43t.com.br>
2021-02-17 16:42:00 -0300

    alt servicescontent

--- js/partials.js
+++ js/partials.js
@@ -7,7 +7,10 @@ function loadPartials() {
   $('#ftco-navbar-topbar-container').load('partials/ftco-navbar-topbar-content.html')
 
   $('#title-container').load('partials/title-content.html')
+
   $('#footer-container').load('partials/footer-content.html')
+
+  $('#services-container').load('partials/services-content.html')
 
   // $('#footer-content').load('partials/footer-content.html')
 
