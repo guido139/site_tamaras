c48a28b11bdc9f86bb70d392481226c113e53087
Gustavo Romero <gustavo@43t.com.br>
2021-02-17 10:00:46 -0300

    Primeiro parcial

---
+++ js/partials.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", loadPartials);
+
+function loadPartials() {
+
+
+  $('#ftco-navbar-container').load('partials/ftco-navbar-container.html')
+
+  // $('#header-navbar').load('partials/header-navbar.html')
+
+  // $('#footer-content').load('partials/footer-content.html')
+
+
+
+}
