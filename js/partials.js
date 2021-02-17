e60a9a3eb618da3b9ac45ebc60afb4b004e5a37c
Gustavo Romero <gustavo@43t.com.br>
2021-02-17 17:58:25 -0300

    xxx

--- js/partials.js
+++ js/partials.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", loadPartials);
 function loadPartials() {
 
 
-  $('#title-container').load('partials/title-content.html')
+  // $('#title-container').load('partials/title-content.html')
 
-  $('#ftco-navbar-topbar-container').load('partials/ftco-navbar-topbar-content.html')
+  // $('#ftco-navbar-topbar-container').load('partials/ftco-navbar-topbar-content.html')
 
 
-  $('#footer-container').load('partials/footer-content.html')
+  // $('#footer-container').load('partials/footer-content.html')
 
   // $('#footer-content').load('partials/footer-content.html')
 
